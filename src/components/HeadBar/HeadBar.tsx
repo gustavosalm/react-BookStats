d5e10bfab96b02b3727c675e976c662f49356e26
gustavosalm <gustavosalmeida9821@gmail.com>
2024-11-09 16:17:44 -0300

    feat: filtering books by searching

--- src/components/HeadBar/HeadBar.tsx
+++ src/components/HeadBar/HeadBar.tsx
@@ -1,10 +1,33 @@
-import { TextField } from "@mui/material";
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import styles from './HeadBar.module.css';
+import React from "react";
 
-const HeadBar = () => {
+type headBarProps = {
+    searchHandler: React.KeyboardEventHandler
+}
+
+const HeadBar: React.FC<headBarProps> = ({ searchHandler }) => {
+
     return (
         <div className={styles.outerDiv}>
-            <TextField fullWidth label="Buscar livro" variant="filled" type="search" className={styles.searchInput} />
+            <TextField
+                fullWidth
+                label="Buscar livro"
+                variant="filled"
+                type="search"
+                className={styles.searchInput}
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
+                onKeyDown={searchHandler}
+            />
         </div>
     )
 }
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import { getBooks } from '../../services/booksService';
+import { getBooks, getBooksByAuthor, getBooksByTitle } from '../../services/booksService';
 import { useState, useEffect } from 'react';
 import HeadBar from '../../components/HeadBar/HeadBar';
 import { Books } from '../../services/booksInterface';
@@ -8,33 +8,49 @@ import styles from './styles.module.css'
 const MainPage = () => {
     const [books, setBooks] = useState<Books[]>([]);
     const [filter, setFilter] = useState('');
-
+    
     useEffect(() => {
-        setNewsList()
+        getBooksList()
     }, [filter]);
 
-    const setNewsList = async () => {
+    const _handleKeyDown = async (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            const searchText = (e.target as HTMLInputElement).value
+            if (searchText === '')
+                return;
+            const titleResponse = await getBooksByTitle(searchText);
+            const authorResponse = await getBooksByAuthor(searchText);
+            const fullList = titleResponse.data.items.concat(authorResponse.data.items);
+            setBooksList(fullList);
+        }
+    }
+
+    const getBooksList = async () => {
         try {
             const response = await getBooks();
-            const newData = response.data.items.map((item: any) => ({
-                id: item.id,
-                volumeInfo: {
-                    authors: item.volumeInfo.authors,
-                    imageLinks: item.volumeInfo.imageLinks,
-                    title: item.volumeInfo.title,
-                    averageRating: item.volumeInfo.averageRating,
-                    categories: item.volumeInfo.categories
-                }
-            } as Books));
-            setBooks(newData);
+            setBooksList(response.data.items);
         } catch(error) {
             console.log(error)
         }
     }
 
+    const setBooksList = (responseItems: any) => {
+        const newData = responseItems.map((item: any) => ({
+            id: item.id,
+            volumeInfo: {
+                authors: item.volumeInfo.authors,
+                imageLinks: item.volumeInfo.imageLinks,
+                title: item.volumeInfo.title,
+                averageRating: item.volumeInfo.averageRating,
+                categories: item.volumeInfo.categories
+            }
+        } as Books));
+        setBooks(newData);
+    }
+
     return (
         <>
-            <HeadBar />
+            <HeadBar searchHandler={_handleKeyDown} />
             <div className={styles.bookListContainer}>
                 {books.map((book, ind) => {
                     return (
