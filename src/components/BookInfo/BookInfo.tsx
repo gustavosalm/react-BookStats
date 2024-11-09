d2eb6fb772e7c25376ed4f736eebd0f0a2cb4091
gustavosalm <gustavosalmeida9821@gmail.com>
2024-11-09 13:00:54 -0300

    fix: preventing error for undefined properties

--- src/components/BookInfo/BookInfo.tsx
+++ src/components/BookInfo/BookInfo.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Books } from '../../services/booksInterface';
 import styles from './BookInfo.module.css';
 
@@ -9,18 +10,18 @@ const BookInfo: React.FC<bookProps> = ({book}) => {
 
     return (
         <div className={styles.bookItem}>
-            <img src={book.volumeInfo.imageLinks.smallThumbnail} />
+            <img src={(book.volumeInfo.imageLinks == undefined) ? 'src/assets/no_image.jpg' : book.volumeInfo.imageLinks.smallThumbnail} />
             <div className={styles.bookInfos}>
                 <h2>{book.volumeInfo.title}</h2>
-                <p>{book.volumeInfo.authors.map((author) => {
+                <p>{book.volumeInfo.authors?.map((author) => {
                     return ' ' + author
                 })}</p>
                 <div className={styles.bookRate} style={{'--rating': (book.volumeInfo.averageRating/5)} as React.CSSProperties}>
                     <div className={styles.stars}>⭐⭐⭐⭐⭐</div>
                 </div>
                 <div className={styles.categoriesDiv}>
-                    {book.volumeInfo.categories.map((category) => {
-                        return (<p className={styles.category}>{category}</p>)
+                    {book.volumeInfo.categories?.map((category, ind) => {
+                        return (<span key={ind} className={styles.category}>{category}</span>)
                     })}
                 </div>
             </div>
