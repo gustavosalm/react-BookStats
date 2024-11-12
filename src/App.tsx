d142f55708b2fb8195e2c8e1fbbf44cb3338cd37
gustavosalm <gustavosalmeida9821@gmail.com>
2024-11-11 22:32:14 -0300

    feat: created variables and context to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from './pages/MainPage'
 import Dashboard from './pages/Dashboard'
+import { useState } from "react";
+import { ThemeContext } from "./context/index";
 
 function App() {
-
+  const [theme, setTheme] = useState('light');
+  
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <ThemeContext.Provider value={{theme, saveTheme: setTheme}} >
+      <div data-theme={theme}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
